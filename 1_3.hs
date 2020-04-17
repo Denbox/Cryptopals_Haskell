5503d5c035b65a05970414dd21dfd898f41d2c5d
Jacob Denbeaux <denbeaux@mit.edu>
2020-04-17 15:38:41 -0400

    completed problems 3 and 4

--- 1_3.hs
+++ 1_3.hs
@@ -2,18 +2,20 @@ import Utils
 import Data.Char
 import Data.List
 
-score :: [Char] -> Int
-score string = sum (map (\x -> if elem x "aeiouAEIOUsStTrRnN " then 1 else 0) string)
+third :: (a, b, c) -> c
+third (a, b, c) = c
 
-compare_snd x y = compare (snd x) (snd y)
+-- compare_snd x y = compare (snd x) (snd y)
+compare_score x y = compare (third x) (third y)
 
 main = do
   let input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
   let binary = hexToBin input
-  let possible_keys = map ((zpadBin 8) . intToBin) [32..126]
+  let possible_keys = map ((zpad 8) . intToBin) [32..126]
   let results = map (\key -> xor binary (cycle key)) possible_keys
   let readable_results = map binToAscii results
-  let scores = map score readable_results
-  let results_and_scores = zip readable_results scores
-  let best = fst $ maximumBy compare_snd results_and_scores
-  print best
+  let readable_keys = map binToAscii possible_keys
+  let scores = map englishScore readable_results
+  let results_keys_scores = zip3 readable_results readable_keys scores
+  let ordered_results = sortBy compare_score results_keys_scores
+  print $ last ordered_results
--- Utils.hs
+++ Utils.hs
@@ -5,9 +5,11 @@ module Utils
 , binToHex
 , binToB64
 , binToAscii
+, asciiToBin
 , xor
-, zpadBin
+, zpad
 , splitLen
+, englishScore
 ) where
 
 import Data.Char
@@ -25,40 +27,25 @@ intToBin 0 = [0]
 intToBin 1 = [1]
 intToBin x = intToBin (quot x 2) ++ [rem x 2]
 
-zpadBin :: Int -> [Int] -> [Int]
-zpadBin len bin = if length bin < len then zpadBin len (0:bin) else bin
+zpad :: Int -> [Int] -> [Int]
+zpad len bin = if length bin < len then zpad len (0:bin) else bin
 
 hexToBin :: [Char] -> [Int]
 hexToBin [] = []
-hexToBin (x:xs) = zpadBin 4 (intToBin $ hexToInt x) ++ hexToBin xs
+hexToBin (x:xs) = zpad 4 (intToBin $ hexToInt x) ++ hexToBin xs
 
 binToInt :: [Int] -> Int
 binToInt [] = 0
 binToInt bits = sum [fst x * (2^(snd x)) | x <- zip (reverse bits) [0..]]
 
-split4 :: [Int] -> [[Int]]
-split4 [] = []
-split4 (a:b:c:d:xs) = [[a,b,c,d]] ++ split4 xs
-split4 lessThan4 = [lessThan4 ++ replicate (4 - length lessThan4) 0]
-
-split6 :: [Int] -> [[Int]]
-split6 [] = []
-split6 (a:b:c:d:e:f:xs) = [[a,b,c,d,e,f]] ++ split6 xs
-split6 lessThan6 = [lessThan6 ++ replicate (6 - length lessThan6) 0]
-
 splitLen :: Int -> [a] -> [[a]]
 splitLen _ [] = []
-splitLen n list
-  | n <= 0 = [list]
-  | otherwise = [take n list] ++ splitLen n (drop n list)
---
--- pad_right_to :: Int -> a -> [a] -> [a]
--- pad_to n val list = list
+splitLen n list = [take n list] ++ splitLen n (drop n list)
 
 binToHex :: [Int] -> [Char]
 binToHex bits =
   let
-    ints = map binToInt $ split4 bits
+    ints = map binToInt $ splitLen 4 bits
     hex = map (\x -> hexEncoding !! x) ints
     -- end_pad_len = mod (length hex) FIGURE OUT HOW END PAD WORKS
   in
@@ -67,7 +54,7 @@ binToHex bits =
 binToB64 :: [Int] -> [Char]
 binToB64 bits =
   let
-    ints = map binToInt $ split6 bits
+    ints = map binToInt $ splitLen 6 bits
     b64 = map (\x -> b64Encoding!!x) ints
     end_pad_len = mod (length b64) 4
     end_pad = concat (replicate end_pad_len "=")
@@ -82,5 +69,12 @@ binToAscii bits =
   in
     ascii
 
+asciiToBin :: [Char] -> [Int]
+asciiToBin [] = []
+asciiToBin (x:xs) = ((zpad 8) . intToBin . ord) x ++ asciiToBin xs
+
 xor :: [Int] -> [Int] -> [Int]
 xor a b = zipWith (\x y -> if x /= y then 1 else 0) a b
+
+englishScore :: [Char] -> Int
+englishScore string = sum (map (\x -> if elem x "ETAOINSHRDLUetaoinshrdlu " then 1 else 0) string)
